a432d083fd4d66c2245aab302f7f74ddffb16bd4
anwanfei <546513287@qq.com>
2020-06-08 17:09:12 +0800

    刷新、导航

--- lib/ui/webview_screen.dart
+++ lib/ui/webview_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 //import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class WebViewScreen extends StatefulWidget {
@@ -13,17 +16,35 @@ class WebViewScreen extends StatefulWidget {
   }
 }
 
+//class _WebViewScreen extends State<WebViewScreen> {
+//  @override
+//  Widget build(BuildContext context) {
+//    var title = widget.title;
+//    var url = widget.url;
+////    return WebviewScaffold(
+////      url: url,
+////      appBar: AppBar(
+////        title: title,
+////      ),
+////    );
+//  return Text(title);
+//  }
+//}
 class _WebViewScreen extends State<WebViewScreen> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
     var title = widget.title;
     var url = widget.url;
-//    return WebviewScaffold(
-//      url: url,
-//      appBar: AppBar(
-//        title: title,
-//      ),
-//    );
-  return Text(title);
+
+    return WebView(
+      initialUrl: url,
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (WebViewController webViewController) {
+        _controller.complete(webViewController);
+      },
+    );
   }
 }
